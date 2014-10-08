de0e5fd42de2d07c751562de5cbea79819c2dcbf
pyro2927 <joseph@pintozzi.com>
2014-10-07 19:33:14 -0500

    Adding seconds-to-words time helper

--- app.rb
+++ app.rb
@@ -25,6 +25,28 @@ helpers do
   def current_user
     !session[:uid].nil?
   end
+
+  def time_in_words(seconds)
+    case seconds
+    when 0..60
+      "#{seconds        } seconds"
+    when 60..3600
+      value = seconds/60
+      "#{value} minute#{value > 1 ? 's' : ''}"
+    when 3600..86400
+      value = seconds/3600
+      "#{value} hour#{  value > 1 ? 's' : ''}"
+    when 86400..604800
+      value = seconds/86400
+      "#{value} day#{   value > 1 ? 's' : ''}"
+    when 604800..2419200
+      value = seconds/604800
+      "#{value} week#{  value > 1 ? 's' : ''}"
+    else
+      nil
+    end
+  end
+
 end
 
 before do
